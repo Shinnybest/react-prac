9bd2a6a1f163ca1430a8b058546c8fda52d52c1a
Shinnybest <kim295521@naver.com>
2022-12-19 23:59:09 +0900

    TotalLike 컴포넌트 추가
    - useState 사용해서 상태 관리

---
+++ src/components/TotalLike.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Like from "./Like";
+
+export default function TotalLike() {
+    const [cnt, setCnt] = useState(0);
+    const handleClick = () => setCnt((prev) => prev + 1)
+
+    return (
+    <div>
+        <span> total like : {cnt} {cnt > 10 ? 'hot' : 'good'}</span>
+        <Like 
+            onClick={handleClick}
+        />
+        <Like 
+            onClick={handleClick}
+        />
+    </div>
+    );
+}
